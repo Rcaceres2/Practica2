5e74924145740490dcb4a6adeed6ad7b6bc82b09
Rcaceres2 <reynaldocaceres0@gmail.com>
2025-06-17 00:35:21 -0400

    first commit

---
+++ Practica2/proyecto2.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+struct Producto {
+    int codigo, stock;
+    char nombre[30], categoria[30];
+    float precio;
+    bool activo;
+};
+
+int main(){
+}
